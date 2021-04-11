88c278394365103a9b6c8c801e3ccedc87cd6d41
Marcel Marques <marcmatiasdev@gmail.com>
2021-04-11 10:01:30 -0300

    Adicao de atividades desenvolvidas ate aula 9

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,45 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>{{ greet }} {{ name }}</div>
+  <div v-html="lastName"></div>
+  <div v-text="nickName"></div>
+  <div v-html="webpage"></div>
+  <h2 :id="headingId">Heading</h2>
+  <button :disabled="isDisabled">Button</button>
+  <h2 class="underline">Underline text</h2>
+  <h2 :class="status">Status</h2>
+  <h2 :class="isSoldout ? 'success' : 'danger'">Sold Out</h2>
+  <h2 :class="[isPromoted && 'promoted', isSoldout ? 'success' : 'danger']">
+    Is Promoted
+  </h2>
+  <h2
+    :class="{
+      promoted: isPromoted,
+      success: isSoldout,
+      danger: !isSouldOut,
+    }"
+  >
+    Object Conditional
+  </h2>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  name: "App",
+  data() {
+    return {
+      greet: "Hello",
+      name: "Marcel",
+      lastName: "<b>Marques</b>",
+      nickName: "@marcmatias",
+      webpage: `<a href='https://marcmatias.github.io'>marcmatias.github.io</a>`,
+      headingId: "heading",
+      isDisabled: true,
+      status: "success",
+      isSoldout: false,
+      isPromoted: true,
+    };
+  },
+};
 </script>
 
 <style>
@@ -22,5 +50,17 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+.underline {
+  text-decoration: underline;
+}
+.danger {
+  color: red;
+}
+.success {
+  color: blue;
+}
+.promoted {
+  font-style: italic;
 }
 </style>
