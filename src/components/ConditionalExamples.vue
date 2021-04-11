63aeb0e75a40416beac9634e42fd1244098c0a6a
Marcel Marques <marcmatiasdev@gmail.com>
2021-04-11 15:47:53 -0300

    Adding components, atividades até aula 13

---
+++ src/components/ConditionalExamples.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <h2 v-if="num === 0">The number is zero</h2>
+    <h2 v-else-if="num > 0">The number is positive</h2>
+    <h2 v-else-if="num < 0">The number is negative</h2>
+    <h2 v-else>Not a number</h2>
+    <h2 v-show="display === true">v-show</h2>
+    <h2 v-if="display === true">v-if</h2>
+  </div>
+  <template v-if="display === true">
+    <h2>@marcmatias</h2>
+    <h2>Example</h2>
+    <h2>Hide elements</h2>
+  </template>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      num: "Hello",
+      display: true,
+    };
+  },
+};
+</script>
+
+<style>
+</style>
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,81 @@
+<template>
+  <div>{{ greet }} {{ name }}</div>
+  <div v-html="lastName"></div>
+  <div v-text="nickName"></div>
+  <div v-html="webpage"></div>
+  <h2 :id="headingId">Heading</h2>
+  <button :disabled="isDisabled">Button</button>
+  <h2 class="underline">Underline text</h2>
+  <h2 :class="status">Status</h2>
+  <h2 :class="isSoldout ? 'success' : 'danger'">Sold Out</h2>
+  <h2 :class="[isPromoted && 'promoted', isSoldout ? 'success' : 'danger']">
+    Is Promoted
+  </h2>
+  <h2
+    :class="{
+      promoted: isPromoted,
+      success: isSoldout,
+      danger: !isSouldOut,
+    }"
+  >
+    Object Conditional
+  </h2>
+  <h2 :style="styleObj">Style Obj</h2>
+  <h2 :style="[styleObj, styleObjBg]">Style Objects</h2>
+</template>
+
+
+<script>
+export default {
+  name: "HelloWorld",
+  data() {
+    return {
+      greet: "Hello",
+      name: "Marcel",
+      lastName: "<b>Marques</b>",
+      nickName: "@marcmatias",
+      webpage: `<a href='https://marcmatias.github.io'>marcmatias.github.io</a>`,
+      headingId: "heading",
+      isDisabled: true,
+      status: "success",
+      isSoldout: false,
+      isPromoted: true,
+      styleObj: {
+        "font-size": "35px",
+        color: "lightgray",
+        fontStyle: "italic",
+      },
+      styleObjBg: {
+        "background-color": "gray",
+        padding: "25px",
+        "margin-left": "100px",
+        "margin-right": "100px",
+      },
+    };
+  },
+};
+</script>
+
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+.underline {
+  text-decoration: underline;
+}
+.danger {
+  color: red;
+}
+.success {
+  color: blue;
+}
+.promoted {
+  font-style: italic;
+}
+</style>
