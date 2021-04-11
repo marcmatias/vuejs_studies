b087145171abdecb2b8657dbee726d92b4211329
Marcel Marques <marcmatiasdev@gmail.com>
2021-04-11 15:47:07 -0300

    Update App component to work with another components

--- src/App.vue
+++ src/App.vue
@@ -1,49 +1,28 @@
 <template>
-  <div>{{ greet }} {{ name }}</div>
-  <div v-html="lastName"></div>
-  <div v-text="nickName"></div>
-  <div v-html="webpage"></div>
-  <h2 :id="headingId">Heading</h2>
-  <button :disabled="isDisabled">Button</button>
-  <h2 class="underline">Underline text</h2>
-  <h2 :class="status">Status</h2>
-  <h2 :class="isSoldout ? 'success' : 'danger'">Sold Out</h2>
-  <h2 :class="[isPromoted && 'promoted', isSoldout ? 'success' : 'danger']">
-    Is Promoted
-  </h2>
-  <h2
-    :class="{
-      promoted: isPromoted,
-      success: isSoldout,
-      danger: !isSouldOut,
-    }"
-  >
-    Object Conditional
-  </h2>
+  <div id="index">
+    <h1 id="title">Index</h1>
+
+    <!-- Components -->
+    <!-- <HelloWorld /> -->
+    <!-- <ConditionalExample /> -->
+  </div>
 </template>
 
 <script>
+// import HelloWorld from "./components/HelloWorld.vue";
+// import ConditionalExample from "./components/ConditionalExamples.vue";
+
 export default {
   name: "App",
-  data() {
-    return {
-      greet: "Hello",
-      name: "Marcel",
-      lastName: "<b>Marques</b>",
-      nickName: "@marcmatias",
-      webpage: `<a href='https://marcmatias.github.io'>marcmatias.github.io</a>`,
-      headingId: "heading",
-      isDisabled: true,
-      status: "success",
-      isSoldout: false,
-      isPromoted: true,
-    };
+  components: {
+    // HelloWorld
+    // ConditionalExample,
   },
 };
 </script>
 
 <style>
-#app {
+#index {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
@@ -51,16 +30,7 @@ export default {
   color: #2c3e50;
   margin-top: 60px;
 }
-.underline {
-  text-decoration: underline;
-}
-.danger {
-  color: red;
-}
-.success {
-  color: blue;
-}
-.promoted {
-  font-style: italic;
+#title {
+  margin-bottom: 60px;
 }
 </style>
