38f277cecccba70d0cef0deab2795e087e48c387
Marcel Marques <marcmatiasdev@gmail.com>
2021-04-12 22:57:18 -0300

    Estudos de EventHandling

--- src/App.vue
+++ src/App.vue
@@ -7,7 +7,8 @@
     <!-- <ConditionalExample /> -->
     <!-- <ListExample /> -->
     <!-- <ListConditionalExample /> -->
-    <Methods />
+    <!-- <Methods /> -->
+    <!-- <EventHandling /> -->
   </div>
 </template>
 
@@ -16,7 +17,8 @@
 // import ConditionalExample from "./components/ConditionalExamples.vue";
 // import ListExample from "./components/ListExample.vue";
 // import ListConditionalExample from "./components/ListConditionalExample.vue";
-import Methods from "./components/Methods.vue";
+// import Methods from "./components/Methods.vue";
+// import EventHandling from "./components/EventHandling.vue";
 
 export default {
   name: "App",
@@ -24,18 +26,22 @@ export default {
     // HelloWorld
     // ConditionalExample,
     // ListConditionalExample,
-    Methods,
+    // Methods,
+    // EventHandling,
   },
 };
 </script>
 
 <style>
+body {
+  background-color: rgb(17, 17, 17);
+}
 #index {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
+  color: rgb(241, 241, 241);
   margin-top: 60px;
 }
 #title {
---
+++ src/components/EventHandling.vue
@@ -0,0 +1,37 @@
+<template>
+  <div>
+    <h2>{{ name }}</h2>
+    <button @click="changeName">Change Name</button>
+    <h2>{{ count }}</h2>
+    <button @click="increase(5)">Increase 5</button>
+    <button @click="increase()">+</button>
+    <button @click="decrease()">-</button>
+    <button @click="increase(-5)">Decrease 5</button>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      name: "Marcel",
+      count: 0,
+    };
+  },
+  methods: {
+    changeName(event) {
+      console.log(event);
+      return (this.name = "Batman");
+    },
+    increase(num = 1) {
+      return (this.count += num);
+    },
+    decrease(num = 1) {
+      return (this.count -= num);
+    },
+  },
+};
+</script>
+
+<style>
+</style>
