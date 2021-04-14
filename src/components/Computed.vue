471f153d258c188e998a54f18a978db57e91cc77
Marcel Marques <marcmatiasdev@gmail.com>
2021-04-13 23:13:05 -0300

    Exemplo de diferenca entre methods e computed properties

--- src/components/Computed.vue
+++ src/components/Computed.vue
@@ -2,14 +2,16 @@
   <div>
     <h2>Fullname - {{ firstName }} {{ lastName }}</h2>
     <h2>Computed Fullname - {{ fullName }}</h2>
+    <h2>Method Total - {{ getTotal() }}</h2>
+    <button @click="items.push({ id: 4, title: 'keyboard', price: 500 })">
+      Add Item
+    </button>
     <h2>
       Total -
       {{ items.reduce((total, curr) => (total = total + curr.price), 0) }}
     </h2>
     <h2>Computed Total - {{ total }}</h2>
-    <button @click="items.push({ id: 4, title: 'keyboard', price: 500 })">
-      Add Item
-    </button>
+    <input type="text" v-model="country" />
   </div>
 </template>
 
@@ -36,14 +38,24 @@ export default {
           price: 399,
         },
       ],
+      country: "",
     };
   },
-  methods: {},
+  methods: {
+    getTotal() {
+      console.log("getTotal Method");
+      return this.items.reduce(
+        (total, curr) => (total = total + curr.price),
+        0
+      );
+    },
+  },
   computed: {
     fullName() {
       return `${this.firstName} ${this.lastName}`;
     },
     total() {
+      console.log("Computed Total");
       return this.items.reduce(
         (total, curr) => (total = total + curr.price),
         0
