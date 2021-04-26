c240c55ede91f6cec73607c7e34c208bf1baec88
Marcel Marques <marcmatiasdev@gmail.com>
2021-04-25 21:03:51 -0300

    Event validation aula 35

--- src/components/Popup.vue
+++ src/components/Popup.vue
@@ -1,13 +1,23 @@
 <template>
-    <div>
-        <h2>This is a popup</h2>
-        <button @click="$emit('close', 'Marcel')">Close</button>
-    </div>
+  <div>
+    <h2>This is a popup</h2>
+    <input type="text" v-model="name" />
+    <button @click="$emit('close', name)">Close</button>
+  </div>
 </template>
 
 <script>
-    export default {
-        name: "Popup",
-        emits: ["close"],
-    }
+export default {
+  name: "Popup",
+  emits: {
+    close: (name) => {
+      return !name ? false : true;
+    },
+  },
+  data() {
+    return {
+      name: "",
+    };
+  },
+};
 </script>
